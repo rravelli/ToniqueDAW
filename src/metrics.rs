24a838fdb9117bb00b9ec59e9a3f94445ef18a71
rravelli <remi.ravelli@gmail.com>
2025-08-05 10:42:22 +0200

    feat: support fft in metrics

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,10 +1,11 @@
 use dashmap::DashMap;
+use rustfft::{FftPlanner, num_complex::Complex};
 
 #[derive(Clone)]
 pub struct AudioMetrics {
     peak: [f32; 2],
     rms: [f32; 2],
-    num_sample: usize,
+    pub samples: [Vec<f32>; 2],
 }
 
 impl AudioMetrics {
@@ -12,20 +13,49 @@ impl AudioMetrics {
         Self {
             peak: [0., 0.],
             rms: [0., 0.],
-            num_sample: 0,
+            samples: [vec![], vec![]],
         }
     }
 
     pub fn add_sample(&mut self, value: f32, channel: usize) {
         self.peak[channel] = self.peak[channel].max(value);
         self.rms[channel] += value * value;
-        self.num_sample += 1;
+        self.samples[channel].push(value);
+    }
+
+    pub fn get_fft(&mut self) -> Vec<f32> {
+        let n = self.samples[0].len();
+        let mut planner = FftPlanner::<f32>::new();
+        let fft = planner.plan_fft_forward(n);
+
+        let hann: Vec<f32> = (0..n)
+            .map(|i| 0.5 * (1.0 - (2.0 * std::f32::consts::PI * i as f32 / (n as f32 - 1.0)).cos()))
+            .collect();
+
+        let mut buffer: Vec<Complex<f32>> = self.samples[0]
+            .iter()
+            .zip(hann.iter())
+            .map(|(&x, &w)| Complex::new(x * w, 0.0))
+            .collect();
+
+        fft.process(&mut buffer);
+
+        let window_sum = hann.iter().sum::<f32>();
+
+        let spectrum: Vec<f32> = buffer
+            .iter()
+            .take(n / 2)
+            .map(|c| 20.0 * (c.norm() * 2.0 / window_sum).max(1e-9).log10() / 4. + 10.) // dBFS
+            .collect();
+
+        spectrum
     }
 
     pub fn get_rms(&self) -> [f32; 2] {
+        let num_sample = self.samples[0].len();
         [
-            (self.rms[0] / self.num_sample as f32).sqrt(),
-            (self.rms[1] / self.num_sample as f32).sqrt(),
+            (self.rms[0] / num_sample as f32).sqrt(),
+            (self.rms[1] / num_sample as f32).sqrt(),
         ]
     }
 
